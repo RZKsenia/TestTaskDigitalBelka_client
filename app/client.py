defe4bf692ed89919ef1d168fdcfe19c911de9f4
kseniya <rzksenia@yandex.ru>
2022-10-07 15:51:38 +1000

    Добавлена задержка для обновления страницы после отправки запроса на добавление данных в БД.

--- app/client.py
+++ app/client.py
@@ -4,7 +4,6 @@ import json
 import pandas as pd
 from wtforms import Form, validators, fields
 from waitress import serve
-from flask.sessions import SecureCookieSession
 
 app = Flask(__name__)  # запускем приложение как одиночный модуль
 # Для реализации защиты от атак, основанных на подделке межсайтовых запросов
@@ -13,9 +12,8 @@ app = Flask(__name__)  # запускем приложение как одино
 # шифровальные блоки, которые используются для проверки аутентичности
 # запросов, содержащие данные форм:
 app.config['SECRET_KEY'] = 'Nbkm_Kbyltvfyy22'
-# server_url = 'http://172.17.0.3:8080' # адрес, по которому обращаемся к веб-серверу
-server_url = 'http://127.0.0.1:3002'
-sess = SecureCookieSession() # используем сессию
+server_url = 'http://172.17.0.3:8080' # адрес, по которому обращаемся к веб-серверу
+# server_url = 'http://127.0.0.1:8080'
 
 class FormGetDataForPredict(Form):
     # Форма для получения данных от пользователя:
@@ -73,8 +71,7 @@ def login():
             # Пользователь в БД найден,
             # переходим на страницу отображения всех данных из БД,
             # что были внесены текущим пользователем:
-            sess['user_name'] = user_name
-            return redirect(url_for('display_all_data'))
+            return redirect(url_for('display_all_data')+'?user_name='+user_name)
     else:
         # отображаем страницу, используя шаблон
         return render_template('login_page.html',
@@ -99,98 +96,117 @@ def display_all_data():
     что были внесены активным пользователем в БД
     :return:
     """
-    user_name = sess['user_name']  # получаем имя пользователя для отображения на странице
-    url_to_redirect = server_url + '/api/display'   # адрес запроса к серверу
-    resp = requests.get(url=url_to_redirect)  # отправляем запрос на сервер
+    if 'user_name' in request.args or 'user_name' in request.get_json():
+        if 'user_name' in request.args:
+            user_name = request.args['user_name']  # получаем имя пользователя для отображения на странице
+        elif 'user_name' in request.get_json():
+            user_name = request.get_json()['user_name']
 
-    if 'error' not in resp.json():
-        df = pd.read_json(json.loads(resp.text))  # получаем DataFrame результат запроса
-        if df.shape[0] != 0:
-            df.set_index(['Концентрат', 'Месяц'])
-        else:
-            # если в БД ещё нет для авторизированного пользователя
-               # никаких записей о содержании веществ в железорудном концентрате,
-            # то выводим пустой Dataframe:
-            dt_for_df = {'Концентрат': '',
-                         'Месяц': '',
-                         'Железо': '',
-                         'Кремний': '',
-                         'Алюминий': '',
-                         'Кальций': '',
-                         'Сера': ''
-                         }
-            df = pd.DataFrame(dt_for_df, index=['0'])
+        url_to_redirect = server_url + '/api/display'   # адрес запроса к серверу
+        # отправляем запрос на сервер
+        # Результатом будет текстовая строка, в формате, пригодном для создания JSON:
+        args_to_send = '{"user_name":' + chr(34) + user_name + chr(34) + '}'
+        resp = requests.get(url=url_to_redirect,
+                            json=args_to_send)
+        json_resp = json.loads(resp.text)
 
-        form_lab = LabInfoForm(request.form)  # новый экземпляр формы для отображения на странице
-
-        if form_lab.btn_show_statistic.data:
-            # Показываем статистическую информацию
-            url_to_redirect = server_url + '/api/report'  # адрес для отправки запроса на сервер
-            month = chr(34) + form_lab.field_month.data + chr(34)  # месяц сопровождается кавычками
-            json_args = {'month': month}  # аргумент для запроса
-
-            resp = requests.post(url=url_to_redirect,
-                                 json=json_args)  # отправляем запрос на сервер
-
-            result = json.loads(resp.text)
-
-            if type(result) is dict:
-                # произошла ошибка - пользователь на авторизирован
-                return render_template('res.html',
-                                       user_id=result['error'])
+        if 'error' not in json_resp:
+            df = pd.read_json(resp.text)  # получаем DataFrame результат запроса
+            if df.shape[0] != 0:
+                df.set_index(['Концентрат', 'Месяц'])
             else:
-                df = pd.read_json(json.loads(resp.text))  # получаем DataFrame результат запроса
+                # если в БД ещё нет для авторизированного пользователя
+                   # никаких записей о содержании веществ в железорудном концентрате,
+                # то выводим пустой Dataframe:
+                dt_for_df = {'Концентрат': '',
+                             'Месяц': '',
+                             'Железо': '',
+                             'Кремний': '',
+                             'Алюминий': '',
+                             'Кальций': '',
+                             'Сера': ''
+                             }
+                df = pd.DataFrame(dt_for_df, index=['0'])
 
-                if df.shape[0] == 0:
-                    # если в БД ещё нет для авторизированного пользователя
-                    # никаких записей о содержании веществ в железорудном концентрате,
-                    # то выводим пустой Dataframe:
-                    dt_for_df = {'Концентрат': '',
-                                 'Месяц': '',
-                                 'Железо': '',
-                                 'Кремний': '',
-                                 'Алюминий': '',
-                                 'Кальций': '',
-                                 'Сера': ''
-                                 }
-                    df = pd.DataFrame(dt_for_df, index=['0'])
-                """
-                Отображение страницы, построенной на основе шаблона, со статистической информацией.
-                На страницу передаются через динамические элементы:
-                back_page - ссылка для возврата
-                month - месяц, за который отображается статистика
-                tables - таблицы в виде страницы HTML, полученная из Dataframe: 
-                """
-                return render_template('work_with_statistic.html',
-                                       back_page=url_for('display_all_data') + '?user_name=' + user_name,
-                                       month=month,
-                                       tables=[df.to_html()])
+            form_lab = LabInfoForm(request.form)  # новый экземпляр формы для отображения на странице
 
-        """
-        Отображение страницы, построенной на основе шаблона, со всей информацией, введённой
-        в БД в таблицу lab_info текущим авторизированным пользователем.
-        Через динамические элементы передаются следующие данные на страницу:
-        user_name - имя пользователя, для отображения приветствия на странице 
-        form - форма для ввода концентрации веществ и названия железорудного концентрата
-        login_page - ссылка на страницу авторизации
-        tables - таблицы в виде страницы HTML, полученная из Dataframe:
-        """
-        url_for_post = server_url + '/api/insert_lab_data'  # адрес запроса к серверу
+            if form_lab.btn_show_statistic.data:
+                # Показываем статистическую информацию
+                url_to_redirect = server_url + '/api/report'  # адрес для отправки запроса на сервер
+                month = chr(34) + form_lab.field_month.data + chr(34)  # месяц сопровождается кавычками
+                json_args = {'month': month}  # аргумент для запроса
 
-        return render_template('work_with_data.html',
-                               user_name=user_name,
-                               form=form_lab,
-                               login_page=url_for('logout'),
-                               tables=[df.to_html()],
-                               url_to_redirect=url_for_post)
-    else:
-        # произошла ошибка - попытка не авторизованного доступа
-        # выводим сообщение об ошибке не отдельной странице
-        return render_template('res.html',
-                               user_id=resp.json()['error'])
+                resp = requests.post(url=url_to_redirect,
+                                     json=json_args)  # отправляем запрос на сервер
 
+                result = json.loads(resp.text)
 
+                if type(result) is dict:
+                    # произошла ошибка - пользователь на авторизирован
+                    return render_template('res.html',
+                                           user_id=result['error'])
+                else:
+                    df = pd.read_json(json.loads(resp.text))  # получаем DataFrame результат запроса
+
+                    if df.shape[0] == 0:
+                        # если в БД ещё нет для авторизированного пользователя
+                        # никаких записей о содержании веществ в железорудном концентрате,
+                        # то выводим пустой Dataframe:
+                        dt_for_df = {'Концентрат': '',
+                                     'Месяц': '',
+                                     'Железо': '',
+                                     'Кремний': '',
+                                     'Алюминий': '',
+                                     'Кальций': '',
+                                     'Сера': ''
+                                     }
+                        df = pd.DataFrame(dt_for_df, index=['0'])
+                    """
+                    Отображение страницы, построенной на основе шаблона, со статистической информацией.
+                    На страницу передаются через динамические элементы:
+                    back_page - ссылка для возврата
+                    month - месяц, за который отображается статистика
+                    tables - таблицы в виде страницы HTML, полученная из Dataframe: 
+                    """
+                    return render_template('work_with_statistic.html',
+                                           back_page=url_for('display_all_data') + '?user_name=' + user_name,
+                                           month=month,
+                                           tables=[df.to_html()])
+
+            """
+            Отображение страницы, построенной на основе шаблона, со всей информацией, введённой
+            в БД в таблицу lab_info текущим авторизированным пользователем.
+            Через динамические элементы передаются следующие данные на страницу:
+            user_name - имя пользователя, для отображения приветствия на странице 
+            form - форма для ввода концентрации веществ и названия железорудного концентрата
+            login_page - ссылка на страницу авторизации
+            tables - таблицы в виде страницы HTML, полученная из Dataframe:
+            """
+            url_for_post = server_url + '/api/insert_lab_data'  # адрес запроса к серверу
+
+            return render_template('work_with_data.html',
+                                   user_name=user_name,
+                                   form=form_lab,
+                                   login_page=url_for('logout'),
+                                   tables=[df.to_html()],
+                                   url_to_redirect=url_for_post,
+                                   json_df_value_str=prepare_json_df_for_page_template(df),
+                                   usr=user_name)
+        else:
+            # произошла ошибка - попытка не авторизованного доступа
+            # выводим сообщение об ошибке не отдельной странице
+            return render_template('res.html',
+                                   user_id=resp.json()['error'])
+
+def prepare_json_df_for_page_template(df):
+    """
+    Подготовка JSON для передачи в шаблон страницы
+    для последующей обработки его скриптом
+    :param json_string_from_df:
+    :return:
+    """
+    return json.loads(df.to_json(orient="values"))
 
 if __name__ == '__main__':
-    # serve(app, listen='*:3002')
-    app.run(debug=False, host='0.0.0.0')
+    serve(app, listen='*:3002')
+    # app.run(debug=False, host='0.0.0.0')
